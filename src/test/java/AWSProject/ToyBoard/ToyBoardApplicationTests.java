4d79e36504b369901c38511fce1ee9016e6a107c
jominkyu1 <jominkyu@gmail.com>
2023-08-25 02:50:14 +0900

    0825 프로젝트 생성

---
+++ src/test/java/AWSProject/ToyBoard/ToyBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package AWSProject.ToyBoard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToyBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
